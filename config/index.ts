4e646a85887009b73d6e8afa11914f62674d1aed
liugod <liugod@users.noreply.github.com>
2023-06-20 09:23:01 +0800

    Update index.ts，修改了配置文件

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,18 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = '92e9b11f-a03e-46be-bfa7-b05c691ded71'
+export const API_KEY = 'app-BJqUQPY0w1aCTwWzWJ1eI160'
+
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+  "title": 'web3老baby',
+  "description": '请问我defi问题',
+  "copyright": 'AIxChain',
+  "privacy_policy": '',
+  "default_language": 'zh-Hans'
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = '你是一个区块链专家，也是金融行业的专家.你可以回答各种跟区块链，金融行业相关的问题，但是需要非常严谨,请用中文回答所有答案'
 
 export const API_PREFIX = '/api'
 
